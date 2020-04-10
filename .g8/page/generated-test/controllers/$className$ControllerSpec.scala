66abd730fcd2138567524ed5c3a60ed7569942db
jordanrowe <jordan.rowe@digital.hmrc.gov.uk>
2020-04-10 14:53:44 +0100

    add .g8

---
+++ .g8/page/generated-test/controllers/$className$ControllerSpec.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.CSRFTokenHelper._
+import play.api.test.Helpers._
+import views.html.$className$View
+
+class $className$ControllerSpec extends SpecBase {
+
+  "$className$ Controller" must {
+
+    "return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      val request = FakeRequest(GET, routes.$className$Controller.onPageLoad().url).withCSRFToken
+
+      val result = route(application, request).value
+
+      val view = application.injector.instanceOf[$className$View]
+
+      status(result) mustEqual OK
+
+      contentAsString(result) mustEqual
+        view()(request, messages).toString
+
+      application.stop()
+    }
+  }
+}
