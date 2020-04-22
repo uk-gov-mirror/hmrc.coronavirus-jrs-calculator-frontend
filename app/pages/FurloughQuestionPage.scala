e8dfdd6061e5a55def65cd6f25653bff1ef6cc41
Chris Wright <chrisjameswright@users.noreply.github.com>
2020-04-22 09:26:50 +0100

    Adding cleanup for FurloughQuestionPage (#141)

--- app/pages/FurloughQuestionPage.scala
+++ app/pages/FurloughQuestionPage.scala
@@ -5,12 +5,23 @@
 
 package pages
 
-import models.FurloughQuestion
+import models.{FurloughQuestion, UserAnswers}
 import play.api.libs.json.JsPath
+
+import scala.util.Try
 
 case object FurloughQuestionPage extends QuestionPage[FurloughQuestion] {
 
   override def path: JsPath = JsPath \ toString
 
   override def toString: String = "furloughQuestion"
+
+  override def cleanup(value: Option[FurloughQuestion], userAnswers: UserAnswers): Try[UserAnswers] =
+    value match {
+      case Some(FurloughQuestion.No) =>
+        userAnswers
+          .remove(FurloughEndDatePage)
+      case _ =>
+        super.cleanup(value, userAnswers)
+    }
 }
--- test/pages/FurloughQuestionPageSpec.scala
+++ test/pages/FurloughQuestionPageSpec.scala
@@ -5,7 +5,9 @@
 
 package pages
 
-import models.FurloughQuestion
+import java.time.LocalDate
+
+import models.{FurloughQuestion, UserAnswers}
 import pages.behaviours.PageBehaviours
 
 class FurloughQuestionSpec extends PageBehaviours {
@@ -17,5 +19,20 @@ class FurloughQuestionSpec extends PageBehaviours {
     beSettable[FurloughQuestion](FurloughQuestionPage)
 
     beRemovable[FurloughQuestion](FurloughQuestionPage)
+
+    "remove furlough end date when answered 'No'" in {
+      val initialAnswers = UserAnswers("id")
+        .set(FurloughQuestionPage, FurloughQuestion.Yes)
+        .success
+        .get
+        .set(FurloughEndDatePage, LocalDate.of(2020, 3, 1))
+        .success
+        .get
+
+      val updatedAnswers = initialAnswers.set(FurloughQuestionPage, FurloughQuestion.No).success.value
+
+      updatedAnswers.get(FurloughEndDatePage) must not be defined
+    }
+
   }
 }
