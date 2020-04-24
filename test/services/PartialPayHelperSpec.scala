616ccd93170e42d1e085b941655f349ea2ac2d80
Chris Wright <chrisjameswright@users.noreply.github.com>
2020-04-24 12:47:39 +0100

    Improving test coverage of partial pay helper (#171)

---
+++ test/services/PartialPayHelperSpec.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ */
+
+package services
+
+import base.{CoreDataBuilder, SpecBase}
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+class PartialPayHelperSpec extends SpecBase with ScalaCheckPropertyChecks with CoreDataBuilder {
+
+  "PartialPayHelper" when {
+
+    "getPeriodRemainder" must {
+
+      "return remainders with multiple days before the partial" in new PartialPayHelper {
+
+        val partial = partialPeriod(
+          "2020,4,5"  -> "2020,4,25",
+          "2020,4,10" -> "2020,4,25"
+        )
+        val expected = period(
+          "2020,4,5",
+          "2020,4,9"
+        )
+
+        getPeriodRemainder(partial) mustBe expected
+      }
+
+      "return remainders with multiple days after the partial" in new PartialPayHelper {
+
+        val partial = partialPeriod(
+          "2020,4,5" -> "2020,4,25",
+          "2020,4,5" -> "2020,4,15"
+        )
+        val expected = period(
+          "2020,4,16",
+          "2020,4,25"
+        )
+
+        getPeriodRemainder(partial) mustBe expected
+      }
+
+      "return remainders with a single day before the partial" in new PartialPayHelper {
+
+        val partial = partialPeriod(
+          "2020,4,5" -> "2020,4,25",
+          "2020,4,6" -> "2020,4,25"
+        )
+        val expected = period(
+          "2020,4,5",
+          "2020,4,5"
+        )
+
+        getPeriodRemainder(partial) mustBe expected
+      }
+
+      "return remainders with a single day after the partial" in new PartialPayHelper {
+
+        val partial = partialPeriod(
+          "2020,4,5" -> "2020,4,25",
+          "2020,4,5" -> "2020,4,24"
+        )
+        val expected = period(
+          "2020,4,25",
+          "2020,4,25"
+        )
+
+        getPeriodRemainder(partial) mustBe expected
+      }
+
+    }
+
+  }
+
+}
