d8aa02c78f94e0e84ea671bca5a33b65fe39bb60
Flavian Alexandru <alexandruflavian23@gmail.com>
2020-05-01 12:05:17 +0100

    CJRSC-4: Update date formatter to include full month name and day

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -6,10 +6,12 @@
 package views
 
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.time.format.{DateTimeFormatter, FormatStyle}
+import java.util.Locale
 
 import play.api.data.Form
 import play.api.i18n.Messages
+import sun.util.resources.LocaleData
 
 object ViewUtils {
 
@@ -24,9 +26,12 @@ object ViewUtils {
   def errorPrefix(form: Form[_])(implicit messages: Messages): String =
     if (form.hasErrors || form.hasGlobalErrors) messages("error.browser.title.prefix") else ""
 
+  private val dateWithMonthNameFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.UK)
   private val dateFormatter = DateTimeFormatter.ofPattern("d MMMM yyyy")
   private val dateFormatterWithoutYear = DateTimeFormatter.ofPattern("d MMMM")
   private val dateFormatterOnlyMonth = DateTimeFormatter.ofPattern("MMMM")
+
+  def dateToReadableString(date: LocalDate): String = dateWithMonthNameFormat.format(date)
   def dateToString(date: LocalDate): String = dateFormatter.format(date)
   def dateToStringWithoutYear(date: LocalDate): String = dateFormatterWithoutYear.format(date)
   def dateToStringOnlyMonth(date: LocalDate): String = dateFormatterOnlyMonth.format(date)
