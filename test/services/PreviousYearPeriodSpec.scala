a7305186c869163817148649a177cbdcee877e74
Chris Wright <chrisjameswright@users.noreply.github.com>
2020-05-04 01:03:03 +0100

    Refactor of CylbOperators

--- test/services/PreviousYearPeriodSpec.scala
+++ test/services/PreviousYearPeriodSpec.scala
@@ -6,7 +6,7 @@
 package services
 
 import base.{CoreTestDataBuilder, SpecBase}
-import models.CylbOperators
+import models.{CylbDuration, CylbOperators}
 import models.PaymentFrequency.{FortNightly, FourWeekly, Monthly, Weekly}
 
 class PreviousYearPeriodSpec extends SpecBase with CoreTestDataBuilder {
@@ -35,6 +35,65 @@ class PreviousYearPeriodSpec extends SpecBase with CoreTestDataBuilder {
     operators(Monthly, fullPeriod("2020,3,1", "2020,3,31")) mustBe CylbOperators(31, 0, 31)
     operators(Monthly, fullPeriod("2020,4,1", "2020,4,30")) mustBe CylbOperators(30, 0, 30)
     operators(Monthly, partialPeriod("2020,3,1" -> "2020,3,31", "2020,3,3" -> "2020,3,31")) mustBe CylbOperators(31, 0, 29)
+  }
+
+  def extract(duration: CylbDuration): (Int, Int, Int) =
+    (duration.fullPeriodLength, duration.equivalentPeriodDays, duration.previousPeriodDays)
+
+  "Weekly tests" in {
+    extract(CylbDuration(Weekly, fullPeriod("2020,3,1", "2020,3,7"))) mustBe Tuple3(7, 5, 2)
+
+    extract(CylbDuration(Weekly, partialPeriod("2020,3,1" -> "2020,3,7", "2020,3,3" -> "2020,3,7"))) mustBe
+      Tuple3(7, 5, 0)
+
+    extract(CylbDuration(Weekly, partialPeriod("2020,3,1" -> "2020,3,7", "2020,3,4" -> "2020,3,7"))) mustBe
+      Tuple3(7, 4, 0)
+
+    extract(CylbDuration(Weekly, partialPeriod("2020,3,1" -> "2020,3,7", "2020,3,2" -> "2020,3,7"))) mustBe
+      Tuple3(7, 5, 1)
+
+    extract(CylbDuration(Weekly, partialPeriod("2020,3,1" -> "2020,3,7", "2020,3,1" -> "2020,3,6"))) mustBe
+      Tuple3(7, 4, 2)
+
+    extract(CylbDuration(Weekly, partialPeriod("2020,3,1" -> "2020,3,7", "2020,3,1" -> "2020,3,2"))) mustBe
+      Tuple3(7, 0, 2)
+  }
+
+  "Fortnightly tests" in {
+    extract(CylbDuration(FortNightly, fullPeriod("2020,3,1", "2020,3,14"))) mustBe
+      Tuple3(14, 12, 2)
+
+    extract(CylbDuration(FortNightly, partialPeriod("2020,3,1" -> "2020,3,14", "2020,3,3" -> "2020,3,14"))) mustBe
+      Tuple3(14, 12, 0)
+
+    extract(CylbDuration(FortNightly, partialPeriod("2020,3,1" -> "2020,3,14", "2020,3,5" -> "2020,3,14"))) mustBe
+      Tuple3(14, 10, 0)
+
+    extract(CylbDuration(FortNightly, partialPeriod("2020,3,1" -> "2020,3,14", "2020,3,2" -> "2020,3,14"))) mustBe
+      Tuple3(14, 12, 1)
+  }
+
+  "Fourweekly tests" in {
+    extract(CylbDuration(FourWeekly, fullPeriod("2020,3,1", "2020,3,28"))) mustBe
+      Tuple3(28, 26, 2)
+
+    extract(CylbDuration(FourWeekly, partialPeriod("2020,3,1" -> "2020,3,28", "2020,3,3" -> "2020,3,28"))) mustBe
+      Tuple3(28, 26, 0)
+
+    extract(CylbDuration(FourWeekly, partialPeriod("2020,3,1" -> "2020,3,28", "2020,3,9" -> "2020,3,28"))) mustBe
+      Tuple3(28, 20, 0)
+
+    extract(CylbDuration(FourWeekly, partialPeriod("2020,3,1" -> "2020,3,28", "2020,3, 2" -> "2020,3,28"))) mustBe
+      Tuple3(28, 26, 1)
+  }
+
+  "Monthly tests" in {
+    extract(CylbDuration(Monthly, fullPeriod("2020,3,1", "2020,3,31"))) mustBe
+      Tuple3(31, 31, 0)
+    extract(CylbDuration(Monthly, fullPeriod("2020,4,1", "2020,4,30"))) mustBe
+      Tuple3(30, 30, 0)
+    extract(CylbDuration(Monthly, partialPeriod("2020,3,1" -> "2020,3,31", "2020,3,3" -> "2020,3,31"))) mustBe
+      Tuple3(31, 29, 0)
   }
 
 }
