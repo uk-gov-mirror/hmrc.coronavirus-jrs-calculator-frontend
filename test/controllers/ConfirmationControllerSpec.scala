ebb28e300e97d873af4e05ce6e4d5ab81a76d732
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2020-11-04 14:42:50 +0000

    [CJRSC-337][JR] confirmation page for extension

--- test/controllers/ConfirmationControllerSpec.scala
+++ test/controllers/ConfirmationControllerSpec.scala
@@ -30,7 +30,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import services.{AuditService, Threshold}
 import viewmodels.{ConfirmationMetadata, ConfirmationViewBreakdown, PhaseTwoConfirmationViewBreakdown}
-import views.html.{ConfirmationViewWithDetailedBreakdowns, NoNicAndPensionConfirmationView, OctoberConfirmationView, PhaseTwoConfirmationView, SeptemberConfirmationView}
+import views.html.{ConfirmationViewWithDetailedBreakdowns, JrsExtensionConfirmationView, NoNicAndPensionConfirmationView, OctoberConfirmationView, PhaseTwoConfirmationView, SeptemberConfirmationView}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -42,6 +42,7 @@ class ConfirmationControllerSpec extends SpecBaseControllerSpecs with CoreTestDa
   val phaseTwoView = app.injector.instanceOf[PhaseTwoConfirmationView]
   val septView = app.injector.instanceOf[SeptemberConfirmationView]
   val octView = app.injector.instanceOf[OctoberConfirmationView]
+  val extView = app.injector.instanceOf[JrsExtensionConfirmationView]
   val audit = app.injector.instanceOf[AuditService]
 
   val controller = new ConfirmationController(
@@ -55,6 +56,7 @@ class ConfirmationControllerSpec extends SpecBaseControllerSpecs with CoreTestDa
     noNicView,
     septView,
     octView,
+    extView,
     audit,
     navigator)
 
