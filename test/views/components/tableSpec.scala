3a58e1ea0088300b485898f45259a3c78cef7cdd
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2021-03-05 09:35:08 +0000

    [CVSRP-4572] Add view spec and model spec

---
+++ test/views/components/tableSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components
+
+import org.scalatestplus.mockito.MockitoSugar
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.test.FakeRequest
+import play.twirl.api.Html
+import uk.gov.hmrc.govukfrontend.views.html.components.GovukTable
+import viewmodels.{Cell, Header, Row, TableModel}
+import views.html.components.table
+
+class tableSpec extends PlaySpec {
+
+  val govukTableComponent = new GovukTable
+  val tableComponent = new table(govukTableComponent)
+
+  "The HTML table component" must {
+
+    "render the table using the expected GovUK styling" in {
+
+      val viewmodel = TableModel(
+        Some("heading"),
+        Some(
+          Header(
+            Seq(
+              Cell(Html("Header A")),
+              Cell(Html("Header B"))
+            )
+          )),
+        Seq(
+          Row(
+            Seq(
+              Cell(Html("Row 1 Cell A")),
+              Cell(Html("Row 1 Cell B"), classes = "customClass")
+            )
+          ),
+          Row(
+            Seq(
+              Cell(Html("Row 2 Cell A")),
+              Cell(Html("Row 2 Cell B"), classes = "customClass")
+            )
+          ),
+          Row(
+            Seq(
+              Cell(Html("Row 3 Cell A")),
+              Cell(Html("Row 3 Cell B"), classes = "customClass")
+            )
+          )
+        )
+      )
+
+      val expected = govukTableComponent(viewmodel.toGovukTable)
+      val actual = tableComponent(viewmodel)
+
+      actual.body.trim mustBe expected.body.trim
+    }
+  }
+}
