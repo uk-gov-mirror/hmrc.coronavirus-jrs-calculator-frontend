e604be425ad66a7934597959396e0a1febfb1d08
Ladon Jackson <31283663+LadonJackson@users.noreply.github.com>
2021-03-08 11:26:46 +0000

    [CVSRP-4574]: update content for the FurloughStartDate page

---
+++ test/assets/messages/FurloughStartDateMessages.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package assets.messages
+
+object FurloughStartDateMessages {
+
+  val title = "When was this employee furloughed?"
+  val heading = "When was this employee furloughed?"
+  val p1 = "If this employee has been furloughed more than once it is the date they were most recently furloughed."
+  val p2 =
+    "If the employee was not on your payroll on 19 March 2020, in relation to Real Time Information (RTI) Full Payment Submission (FPS), and was furloughed before 1 November 2020, then use the date 1 November 2020."
+  val extensionP2 =
+    "If this employee was not on your payroll on 30 October 2020 and was furloughed before 1 May 2021, then use the date 1 May 2021."
+  val extensionP3 =
+    "Employees must be on your payroll and therefore appear in your Real Time Information (RTI) Full Payment Submissions (FPS) on or before 2 March 2021 to be eligible."
+}
