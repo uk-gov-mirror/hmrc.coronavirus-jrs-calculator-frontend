fe5937fe5d0319cd17ebdc96233f3ac0d7094daa
“BackRowOla” <56401572+BackRowOla@users.noreply.github.com>
2021-03-11 15:31:30 +0000

    updated Unit and it tests

--- test/controllers/FirstFurloughDateControllerSpec.scala
+++ test/controllers/FirstFurloughDateControllerSpec.scala
@@ -18,13 +18,16 @@ package controllers
 
 import java.time.{LocalDate, ZoneOffset}
 
+import assets.messages.PartTimeNormalHoursMessages.dateToString
 import base.SpecBaseControllerSpecs
+import config.featureSwitch.{ExtensionTwoNewStarterFlow, FeatureSwitching}
 import controllers.actions.DataRetrievalActionImpl
 import forms.FirstFurloughDateFormProvider
+import models.EmployeeStarted.OnOrBefore1Feb2019
 import models.PayMethod.Variable
 import models.PaymentFrequency.Weekly
 import models.{EmployeeStarted, UserAnswers}
-import pages.{FirstFurloughDatePage, FurloughStartDatePage}
+import pages.{EmployeeStartedPage, FirstFurloughDatePage, FurloughStartDatePage, OnPayrollBefore30thOct2020Page, PreviousFurloughPeriodsPage}
 import play.api.data.Form
 import play.api.libs.json.Json
 import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded, Call}
@@ -36,12 +39,13 @@ import views.html.FirstFurloughDateView
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class FirstFurloughDateControllerSpec extends SpecBaseControllerSpecs {
+class FirstFurloughDateControllerSpec extends SpecBaseControllerSpecs with FeatureSwitching {
 
-  val formProvider                  = new FirstFurloughDateFormProvider()
-  val validAnswer                   = LocalDate.now(ZoneOffset.UTC)
-  val furlouoghStartDate            = validAnswer.plusMonths(1)
-  private def form: Form[LocalDate] = formProvider(furlouoghStartDate)
+  val formProvider      = new FirstFurloughDateFormProvider()
+  val validAnswer       = LocalDate.now(ZoneOffset.UTC)
+  val firstFurloughDate = validAnswer.plusMonths(1)
+
+  private def form: Form[LocalDate] = formProvider(firstFurloughDate)
 
   lazy val firstFurLoughDateStartRoute: String = routes.FirstFurloughDateController.onPageLoad().url
 
@@ -62,6 +66,14 @@ class FirstFurloughDateControllerSpec extends SpecBaseControllerSpecs {
 
   val view = app.injector.instanceOf[FirstFurloughDateView]
 
+  val nov9th2020            = LocalDate.of(2020, 11, 9)
+  val mar8th2020: LocalDate = LocalDate.of(2020, 3, 8)
+  val may8th2021: LocalDate = LocalDate.of(2021, 5, 8)
+
+  val nov1st2020            = LocalDate.of(2020, 11, 1)
+  val mar1st2020: LocalDate = LocalDate.of(2020, 3, 1)
+  val may1st2021: LocalDate = LocalDate.of(2021, 5, 1)
+
   def controller(stubbedAnswers: Option[UserAnswers] = Some(emptyUserAnswers)) =
     new FirstFurloughDateController(
       messagesApi,
@@ -78,39 +90,41 @@ class FirstFurloughDateControllerSpec extends SpecBaseControllerSpecs {
       view
     )
 
-  "FirstFurloughDate Controller" must {
+  def userAnswersEmployedBefore1stFeb2019(): UserAnswers =
+    UserAnswers(userAnswersId)
+      .withPayMethod(Variable)
+      .withEmployeeStartedOnOrBefore1Feb2019()
+      .set(FurloughStartDatePage, mar8th2020)
+      .success
+      .value
 
-    "return OK and the correct view for a GET" in {
+  def userAnswersEmployedAfter1stFeb2019(isOnPayrollBefore30thOct: Boolean): UserAnswers =
+    UserAnswers(userAnswersId)
+      .withFurloughStartDate(nov9th2020.toString)
+      .withPayMethod(Variable)
+      .withEmployeeStartedAfter1Feb2019()
+      .withEmployeeStartDate("2020,3,20")
+      .set(OnPayrollBefore30thOct2020Page, isOnPayrollBefore30thOct)
+      .success
+      .value
 
-      val userAnswers = UserAnswers(userAnswersId).set(FurloughStartDatePage, furlouoghStartDate).success.value
+  def multiScenario(userAnswers: UserAnswers, resultDate: LocalDate) = {
 
-      val result = controller(Some(userAnswers)).onPageLoad()(getRequest)
+    s"populate the view correctly on a GET when the question has previously been answered for ${dateToString(resultDate)}" in {
 
-      status(result) mustEqual OK
-      contentAsString(result) mustEqual view(form)(getRequest, messages).toString()
-    }
-
-    "populate the view correctly on a GET when the question has previously been answered" in {
-
-      val userAnswers = UserAnswers(userAnswersId)
-        .set(FirstFurloughDatePage, validAnswer)
-        .success
-        .value
-        .set(FurloughStartDatePage, furlouoghStartDate)
-        .success
-        .value
-
-      val result = controller(Some(userAnswers)).onPageLoad()(getRequest)
+      val result = controller(
+        Some(
+          userAnswers
+            .set(FirstFurloughDatePage, resultDate.plusDays(7))
+            .success
+            .value)).onPageLoad()(getRequest)
 
       status(result) mustBe OK
       contentAsString(result) mustEqual
-        view(form.fill(validAnswer))(getRequest, messages).toString()
-
+        view(form.fill(resultDate.plusDays(7)), resultDate)(getRequest, messages).toString()
     }
 
-    "return a Bad Request and errors when invalid data is submitted" in {
-
-      val userAnswers = UserAnswers(userAnswersId).set(FurloughStartDatePage, furlouoghStartDate).success.value
+    s"return a Bad Request and errors when invalid data is submitted for ${dateToString(resultDate)}" in {
 
       val request =
         FakeRequest(POST, firstFurLoughDateStartRoute).withCSRFToken
@@ -123,9 +137,43 @@ class FirstFurloughDateControllerSpec extends SpecBaseControllerSpecs {
 
       status(result) mustEqual BAD_REQUEST
       contentAsString(result) mustEqual
-        view(boundForm)(request, messages).toString()
-
+        view(boundForm, resultDate)(request, messages).toString()
     }
+
+  }
+
+  "FirstFurloughDate Controller" must {
+
+    "return OK and the correct view for a GET" when {
+
+      "the user selected Yes to the employee working before Feb 2019 and yes on the previous furlough periods page" in {
+
+        val result = controller(Some(userAnswersEmployedBefore1stFeb2019())).onPageLoad()(getRequest)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, mar1st2020)(getRequest, messages).toString()
+      }
+
+      "the user selected No to the employee Working before Feb 2019" must {
+        "show 1st November 2020 content when the user answered Yes on OnPayrollBefore30thOct2020Page" in {
+          val result = controller(Some(userAnswersEmployedAfter1stFeb2019(true))).onPageLoad()(getRequest)
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(form, nov1st2020)(getRequest, messages).toString()
+        }
+
+        "show 1st May 2021 content when the user answered No on OnPayrollBefore30thOct2020Page" in {
+          val result = controller(Some(userAnswersEmployedAfter1stFeb2019(false))).onPageLoad()(getRequest)
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(form, may1st2021)(getRequest, messages).toString()
+        }
+      }
+    }
+
+    multiScenario(userAnswersEmployedBefore1stFeb2019(), mar1st2020)
+    multiScenario(userAnswersEmployedAfter1stFeb2019(true), nov1st2020)
+    multiScenario(userAnswersEmployedAfter1stFeb2019(false), may1st2021)
 
     "redirect to Session Expired for a GET if no existing data is found" in {
       val result = controller(None).onPageLoad()(getRequest)
