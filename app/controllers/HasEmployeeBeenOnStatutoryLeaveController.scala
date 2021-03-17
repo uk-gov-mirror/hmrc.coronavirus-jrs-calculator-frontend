3c751621e8ccc778adc7c9672313cabdabfd16ab
Johnny <56835853+peckoverHMRC@users.noreply.github.com>
2021-03-17 16:23:28 +0000

    [CVSRP-4694] create helper and dynamic page wip

--- app/controllers/HasEmployeeBeenOnStatutoryLeaveController.scala
+++ app/controllers/HasEmployeeBeenOnStatutoryLeaveController.scala
@@ -17,49 +17,56 @@
 package controllers
 
 import cats.data.Validated.{Invalid, Valid}
+import config.FrontendAppConfig
 import controllers.actions._
 import forms.HasEmployeeBeenOnStatutoryLeaveFormProvider
-
-import javax.inject.Inject
-import models.Mode
 import navigation.Navigator
 import pages.HasEmployeeBeenOnStatutoryLeavePage
+import play.api.Logger
 import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
+import viewmodels.BeenOnStatutoryLeaveHelper
 import views.html.HasEmployeeBeenOnStatutoryLeaveView
 
+import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class HasEmployeeBeenOnStatutoryLeaveController @Inject()(override val messagesApi: MessagesApi,
-                                                          sessionRepository: SessionRepository,
-                                                          navigator: Navigator,
-                                                          identify: IdentifierAction,
-                                                          getData: DataRetrievalAction,
-                                                          requireData: DataRequiredAction,
-                                                          formProvider: HasEmployeeBeenOnStatutoryLeaveFormProvider,
-                                                          val controllerComponents: MessagesControllerComponents,
-                                                          view: HasEmployeeBeenOnStatutoryLeaveView)(implicit ec: ExecutionContext)
+class HasEmployeeBeenOnStatutoryLeaveController @Inject()(
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: Navigator,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  formProvider: HasEmployeeBeenOnStatutoryLeaveFormProvider,
+  helper: BeenOnStatutoryLeaveHelper,
+  val controllerComponents: MessagesControllerComponents,
+  view: HasEmployeeBeenOnStatutoryLeaveView)(implicit ec: ExecutionContext, appConfig: FrontendAppConfig)
     extends FrontendBaseController with I18nSupport {
 
-  val form: Form[Boolean] = formProvider()
-
   def onPageLoad(): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    Logger.debug(
+      s"[HasEmployeeBeenOnStatutoryLeaveController][onPageLoad] " +
+        s"boundaryStart: ${helper.boundaryStart()}, boundaryEnd: ${helper.boundaryEnd()}")
+    val form: Form[Boolean] = formProvider(helper.boundaryStart(), helper.boundaryEnd())
     val preparedForm = request.userAnswers.getV(HasEmployeeBeenOnStatutoryLeavePage) match {
       case Invalid(_)   => form
       case Valid(value) => form.fill(value)
     }
-
-    Ok(view(preparedForm))
+    val postAction = controllers.routes.HasEmployeeBeenOnStatutoryLeaveController.onSubmit()
+    Ok(view(preparedForm, postAction, helper.boundaryStart(), helper.boundaryEnd()))
   }
 
   def onSubmit(): Action[AnyContent] = (identify andThen getData andThen requireData).async { implicit request =>
+    val postAction          = controllers.routes.HasEmployeeBeenOnStatutoryLeaveController.onSubmit()
+    val form: Form[Boolean] = formProvider(helper.boundaryStart(), helper.boundaryEnd())
     form
       .bindFromRequest()
       .fold(
-        formWithErrors => Future.successful(BadRequest(view(formWithErrors))),
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, postAction, helper.boundaryStart(), helper.boundaryEnd()))),
         value =>
           for {
             updatedAnswers <- Future.fromTry(request.userAnswers.set(HasEmployeeBeenOnStatutoryLeavePage, value))
