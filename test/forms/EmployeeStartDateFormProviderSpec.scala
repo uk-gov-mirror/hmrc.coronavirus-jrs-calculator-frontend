ae8bed838ce0bd6fc6f9df857ef29821b7e8fc27
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2021-03-17 15:18:15 +0000

    [CVSRP-4727] Update validation on employee start date for May 2021 onwards claims

--- test/forms/EmployeeStartDateFormProviderSpec.scala
+++ test/forms/EmployeeStartDateFormProviderSpec.scala
@@ -16,16 +16,16 @@
 
 package forms
 
-import java.time.LocalDate
-
-import play.api.test.CSRFTokenHelper._
 import forms.behaviours.DateBehaviours
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.data.FormError
 import play.api.i18n.{Messages, MessagesApi}
 import play.api.mvc.AnyContentAsEmpty
+import play.api.test.CSRFTokenHelper._
 import play.api.test.FakeRequest
 import views.ViewUtils._
+
+import java.time.LocalDate
 
 class EmployeeStartDateFormProviderSpec extends DateBehaviours with GuiceOneAppPerSuite {
 
@@ -34,33 +34,89 @@ class EmployeeStartDateFormProviderSpec extends DateBehaviours with GuiceOneAppP
     FakeRequest("", "").withCSRFToken.asInstanceOf[FakeRequest[AnyContentAsEmpty.type]]
   implicit val messages: Messages = messagesApi.preferred(fakeRequest)
 
-  val furloughStart = LocalDate.of(2020, 3, 10)
-  val claimStart    = LocalDate.of(2020, 3, 1)
+  lazy val formProvider = new EmployeeStartDateFormProvider()
 
-  val formProvider = new EmployeeStartDateFormProvider()
+  ".value" when {
 
-  val form = formProvider(furloughStart, claimStart)
+    "When the claim is Pre-November" must {
 
-  ".value" should {
+      val furloughStart = LocalDate.of(2020, 3, 10)
+      val claimStart    = LocalDate.of(2020, 3, 1)
+      lazy val form     = formProvider(furloughStart, claimStart)
 
-    val validData = datesBetween(
-      min = formProvider.feb2nd2019,
-      max = furloughStart
-    )
+      val validData = datesBetween(
+        min = formProvider.feb2nd2019,
+        max = furloughStart
+      )
 
-    behave like dateField(form, "value", validData)
+      behave like dateField(form, "value", validData)
 
-    behave like dateFieldWithMax(form,
-                                 "value",
-                                 furloughStart.minusDays(1),
-                                 FormError("value", "employeeStartDate.error.max", Seq("19 March 2020")))
+      behave like dateFieldWithMax(form,
+                                   "value",
+                                   furloughStart.minusDays(1),
+                                   FormError("value", "employeeStartDate.error.max", Seq("19 March 2020")))
 
-    behave like dateFieldWithMin(form,
-                                 "value",
-                                 formProvider.feb2nd2019,
-                                 FormError("value", "employeeStartDate.error.min", Array(dateToString(formProvider.feb2nd2019))))
+      behave like dateFieldWithMin(form,
+                                   "value",
+                                   formProvider.feb2nd2019,
+                                   FormError("value", "employeeStartDate.error.min", Array(dateToString(formProvider.feb2nd2019))))
 
-    behave like mandatoryDateField(form, "value")
+      behave like mandatoryDateField(form, "value")
 
+    }
+
+    "When the claim is November 2020 to April 2021" must {
+
+      val furloughStart = LocalDate.of(2020, 9, 10)
+      val claimStart    = LocalDate.of(2020, 12, 1)
+      val form          = formProvider(furloughStart, claimStart)
+
+      val validData = datesBetween(
+        min = formProvider.feb2nd2019,
+        max = furloughStart
+      )
+
+      behave like dateField(form, "value", validData)
+
+      behave like dateFieldWithMax(form,
+                                   "value",
+                                   furloughStart.minusDays(1),
+                                   FormError("value", "employeeStartDate.error.max", Seq("30 October 2020")))
+
+      behave like dateFieldWithMin(form,
+                                   "value",
+                                   formProvider.feb2nd2019,
+                                   FormError("value", "employeeStartDate.error.min", Array(dateToString(formProvider.feb2nd2019))))
+
+      behave like mandatoryDateField(form, "value")
+
+    }
+
+    "When the claim is May 2021 to September 2021" must {
+
+      val furloughStart = LocalDate.of(2021, 1, 10)
+      val claimStart    = LocalDate.of(2021, 5, 1)
+      val form          = formProvider(furloughStart, claimStart)
+
+      val validData = datesBetween(
+        min = formProvider.feb2nd2019,
+        max = furloughStart
+      )
+
+      behave like dateField(form, "value", validData)
+
+      behave like dateFieldWithMax(form,
+                                   "value",
+                                   furloughStart.minusDays(1),
+                                   FormError("value", "employeeStartDate.error.max", Seq("2 March 2021")))
+
+      behave like dateFieldWithMin(form,
+                                   "value",
+                                   formProvider.feb2nd2019,
+                                   FormError("value", "employeeStartDate.error.min", Array(dateToString(formProvider.feb2nd2019))))
+
+      behave like mandatoryDateField(form, "value")
+
+    }
   }
 }
