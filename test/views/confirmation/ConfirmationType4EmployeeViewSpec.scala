a77f4d559b873bdc09bf98afa61f838a8301e6ca
Johnny <56835853+peckoverHMRC@users.noreply.github.com>
2021-03-25 16:12:04 +0000

    [CVSRP-4765b] correct test for type 4 employee averaging calculation p1

---
+++ test/views/confirmation/ConfirmationType4EmployeeViewSpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.confirmation
+
+import base.SpecBase
+import cats.scalatest.ValidatedValues
+import handlers.ConfirmationControllerRequestHandler
+import messages.JRSExtensionConfirmationMessages.Type4.averageP1
+import models.FurloughStatus.FurloughOngoing
+import models.PartTimeQuestion.PartTimeNo
+import models.PayMethod.Variable
+import models.PaymentFrequency.Monthly
+import models.requests.DataRequest
+import models.{EmployeeStarted, Period, UserAnswers}
+import org.jsoup.nodes.Document
+import play.twirl.api.HtmlFormat
+import utils.ValueFormatter
+import viewmodels.ConfirmationDataResultWithoutNicAndPension
+import views.behaviours.ViewBehaviours
+import views.html.JrsExtensionConfirmationView
+
+import java.time.LocalDate
+
+class ConfirmationType4EmployeeViewSpec
+    extends SpecBase with ConfirmationControllerRequestHandler with ValidatedValues with ValueFormatter with ViewBehaviours {
+
+  val extConfirmationView: JrsExtensionConfirmationView = injector.instanceOf[JrsExtensionConfirmationView]
+
+  val novClaimPeriod: Period = Period(
+    LocalDate.of(2020, 11, 1),
+    LocalDate.of(2020, 12, 1)
+  )
+
+  def nov2020Type5Journey(): UserAnswers =
+    emptyUserAnswers
+      .withClaimPeriodStart("2020, 11, 1")
+      .withClaimPeriodEnd("2020, 11, 30")
+      .withFurloughStartDate("2020, 11, 15")
+      .withFurloughStatus(FurloughOngoing)
+      .withPaymentFrequency(Monthly)
+      .withNiCategory()
+      .withPensionStatus()
+      .withPayMethod(Variable)
+      .withFurloughInLastTaxYear(false)
+      .withVariableLengthEmployed(EmployeeStarted.After1Feb2019)
+      .withEmployeeStartDate("2020, 1, 31")
+      .withPreviousFurloughedPeriodsAnswer(true)
+      .withFirstFurloughDate("2020, 11, 10")
+      .withPayDate(List("2020, 10, 31", "2020, 12, 1"))
+      .withAnnualPayAmount(10000.00)
+      .withPartTimeQuestion(PartTimeNo)
+
+  "display the correct paragraph text for averaging calc" when {
+
+    val userAnswers: UserAnswers = nov2020Type5Journey()
+
+    implicit val request: DataRequest[_] = fakeDataRequest()
+
+    "view is extension confirmation view" in {
+
+      val noNicAndPensionBreakdown = {
+        loadResultData(userAnswers).value.asInstanceOf[ConfirmationDataResultWithoutNicAndPension].confirmationViewBreakdown
+      }
+
+      def applyView(): HtmlFormat.Appendable =
+        extConfirmationView(cvb = noNicAndPensionBreakdown, claimPeriod = novClaimPeriod, version = "2", isNewStarterType5 = false)
+
+      implicit val doc: Document = asDocument(applyView())
+
+      doc.toString.contains(averageP1) mustBe true
+    }
+  }
+}
