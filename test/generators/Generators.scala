04cb4b2178605048034731550382d2ab9d16d640
mywyau <47248104+mywyau@users.noreply.github.com>
2021-03-30 17:18:15 +0100

    undo a bad conflict resolve

--- test/generators/Generators.scala
+++ test/generators/Generators.scala
@@ -110,7 +110,7 @@ trait Generators extends UserAnswersGenerator with PageGenerators with ModelGene
 
   def positiveBigDecimalsWithMaxAnd2DP(maxValue: BigDecimal): Gen[BigDecimal] =
     for {
-      value <- arbitrary[BigDecimal] suchThat (bd => bd.setScale(0, RoundingMode.FLOOR).compareTo(BigDecimal(0)) >= 0.01 && bd < maxValue)
+      value <- arbitrary[BigDecimal] suchThat (bd => bd.setScale(0, RoundingMode.FLOOR).compareTo(BigDecimal(0)) > 0 && bd < maxValue)
     } yield {
       value.setScale(2, RoundingMode.HALF_UP)
     }
